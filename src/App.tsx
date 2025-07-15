4a4551b2a45eef7973a5f272e35b5d0c5d2f80b9
Leonardo dos Santos <leodoima@hotmail.com>
2025-07-15 08:19:20 -0300

    Create project base with Vite, React Flow and Tailwind

---
+++ src/App.tsx
@@ -0,0 +1,42 @@
+import { addEdge, Background, Controls, MiniMap, ReactFlow, useEdgesState, useNodesState, type OnConnect } from '@xyflow/react'
+import { useCallback } from 'react';
+import colors from 'tailwindcss/colors';
+import '@xyflow/react/dist/style.css';
+
+
+const initialNodes = [
+  { id: 'n1', position: { x: 0, y: 0 }, data: { label: 'Node 1' } },
+  { id: 'n2', position: { x: 0, y: 100 }, data: { label: 'Node 2' } },
+];
+const initialEdges = [{ id: 'n1-n2', source: 'n1', target: 'n2' }];
+
+function App() {
+
+  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+
+  const onConnect: OnConnect = useCallback(
+    (params) => setEdges((edgesSnapshot) => addEdge(params, edgesSnapshot)),
+    [],
+  );
+
+  return (
+    <div className="w-screen h-screen">
+      <ReactFlow
+        nodes={nodes}
+        edges={edges}
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+        fitView
+      >
+        <Controls showInteractive={false} />
+        <MiniMap />
+        <Background color={colors.zinc[50]} />
+
+      </ReactFlow>
+    </div>
+  )
+}
+
+export default App
